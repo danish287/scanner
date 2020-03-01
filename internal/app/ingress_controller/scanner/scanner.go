713e33bb6cf8ad323217517fb28983c718e26ba4
Dania Estrada <ale.estrada99@gmail.com>
2020-03-01 15:02:59 -0600

    able torequest services and deployments asn configure to struct

--- internal/app/ingress_controller/scanner/scanner.go
+++ internal/app/ingress_controller/scanner/scanner.go
@@ -35,8 +35,8 @@ type MyDeployments struct {
 	Items []Deployment `json:"items"`
 }
 
-// Deployment 
-type Deployment struct  {
+// Deployment contains
+type Deployment struct {
 	Metadata struct {
 		Name              string    `json:"name"`
 		Namespace         string    `json:"namespace"`
@@ -50,7 +50,52 @@ type Deployment struct  {
 			KubectlKubernetesIoLastAppliedConfiguration string `json:"kubectl.kubernetes.io/last-applied-configuration"`
 		} `json:"annotations"`
 	} `json:"metadata"`
-	Sepc []DeploymentSpec `json:"spec"`
+	Spec struct {
+		Replicas int `json:"replicas"`
+		Selector struct {
+			MatchLabels struct {
+				App string `json:"app"`
+			} `json:"matchLabels"`
+		} `json:"selector"`
+		Template struct {
+			Metadata struct {
+				CreationTimestamp interface{} `json:"creationTimestamp"`
+				Labels            struct {
+					App string `json:"app"`
+				} `json:"labels"`
+			} `json:"metadata"`
+			Spec struct {
+				Containers []struct {
+					Name  string `json:"name"`
+					Image string `json:"image"`
+					Ports []struct {
+						ContainerPort int    `json:"containerPort"`
+						Protocol      string `json:"protocol"`
+					} `json:"ports"`
+					Resources struct {
+					} `json:"resources"`
+					TerminationMessagePath   string `json:"terminationMessagePath"`
+					TerminationMessagePolicy string `json:"terminationMessagePolicy"`
+					ImagePullPolicy          string `json:"imagePullPolicy"`
+				} `json:"containers"`
+				RestartPolicy                 string `json:"restartPolicy"`
+				TerminationGracePeriodSeconds int    `json:"terminationGracePeriodSeconds"`
+				DNSPolicy                     string `json:"dnsPolicy"`
+				SecurityContext               struct {
+				} `json:"securityContext"`
+				SchedulerName string `json:"schedulerName"`
+			} `json:"spec"`
+		} `json:"template"`
+		Strategy struct {
+			Type          string `json:"type"`
+			RollingUpdate struct {
+				MaxUnavailable string `json:"maxUnavailable"`
+				MaxSurge       string `json:"maxSurge"`
+			} `json:"rollingUpdate"`
+		} `json:"strategy"`
+		RevisionHistoryLimit    int `json:"revisionHistoryLimit"`
+		ProgressDeadlineSeconds int `json:"progressDeadlineSeconds"`
+	} `json:"spec"`
 	Status struct {
 		ObservedGeneration  int `json:"observedGeneration"`
 		Replicas            int `json:"replicas"`
@@ -67,53 +112,6 @@ type Deployment struct  {
 	} `json:"status"`
 } 
 
-// DeploymentSpec contains
-type DeploymentSpec  struct {
-	Replicas int `json:"replicas"`
-	Selector struct {
-		MatchLabels struct {
-			App string `json:"app"`
-		} `json:"matchLabels"`
-	} `json:"selector"`
-	Template struct {
-		Metadata struct {
-			CreationTimestamp interface{} `json:"creationTimestamp"`
-			Labels            struct {
-				App string `json:"app"`
-			} `json:"labels"`
-		} `json:"metadata"`
-		Spec struct {
-			Containers []struct {
-				Name  string `json:"name"`
-				Image string `json:"image"`
-				Ports []struct {
-					ContainerPort int    `json:"containerPort"`
-					Protocol      string `json:"protocol"`
-				} `json:"ports"`
-				Resources struct {
-				} `json:"resources"`
-				TerminationMessagePath   string `json:"terminationMessagePath"`
-				TerminationMessagePolicy string `json:"terminationMessagePolicy"`
-				ImagePullPolicy          string `json:"imagePullPolicy"`
-			} `json:"containers"`
-			RestartPolicy                 string `json:"restartPolicy"`
-			TerminationGracePeriodSeconds int    `json:"terminationGracePeriodSeconds"`
-			DNSPolicy                     string `json:"dnsPolicy"`
-			SecurityContext               struct {
-			} `json:"securityContext"`
-			SchedulerName string `json:"schedulerName"`
-		} `json:"spec"`
-	} `json:"template"`
-	Strategy struct {
-		Type          string `json:"type"`
-		RollingUpdate struct {
-			MaxUnavailable string `json:"maxUnavailable"`
-			MaxSurge       string `json:"maxSurge"`
-		} `json:"rollingUpdate"`
-	} `json:"strategy"`
-	RevisionHistoryLimit    int `json:"revisionHistoryLimit"`
-	ProgressDeadlineSeconds int `json:"progressDeadlineSeconds"`
-} 
 
 // Service contains
 type Service struct {
@@ -163,52 +161,86 @@ type Rules struct {
 // Ruleset stores
 var Ruleset []Rules
 
+// ReqDeployments stores
+var ReqDeployments MyDeployments
+
+// ReqServices contians
+var ReqServices MyServices
+
 func main() {
 	// run the kubectl proxy without TLS credentials
 	exec.Command("kubectl", "proxy", "--insecure-skip-tls-verify").Start()
-	GetDeployments()
+	GetServices()
 }
 
 // GetServices gets all of the services in our cluster from the API
 func GetServices() {
 
-	// api/v1/namespaces/revature.com/services/
 	// request information of services from k8s API
-	var reqServices MyServices
-	// var currServices 
 	serviceURL := "http://localhost:8001/api/v1/services"
 	body := GetResponse(serviceURL)
 	
 	// unmarshall body of the request and populate structure currServices with information of current services from K8S API
-	err := json.Unmarshal(body, &reqServices)
+	err := json.Unmarshal(body, &ReqServices)
 	if err != nil { fmt.Println("FIRST ERROR", err)}
-	fmt.Println("\n\nUNMARSHALL", reqServices.Items)
-	fmt.Println("L:ENGHT", len(reqServices.Items))
+	FindService()
 
+	fmt.Println("\n\nUNMARSHALL", ReqServices.Items)
+	fmt.Println("LENGHT", len(ReqServices.Items))
+}
+
+//FindService will
+func FindService() {
+
+	serviceLst := ReqServices.Items
+	for i:=0; i < len(serviceLst); i++{
+		currService := serviceLst[i]
+		// if currService.Name == name we are looking for 
+		// Route.ServiceName = currService..Metadata.Name
+		// Route.ServicePort = currService.Spec.Ports.Port
+		// Route.ServiceIP = currService.Spec.ClusterIP
+		fmt.Println("CURRENTSERVICE", currService.Metadata.Name)
+	}
 }
 
 // GetDeployments contains
 func GetDeployments() {
 	// GET /apis/apps/v1/namespaces/{namespace}/deployments/{name}
 	// request information of services from k8s API
-	var reqDeployments MyDeployments
 	serviceURL := "http://localhost:8001/apis/apps/v1/deployments"
 	body := GetResponse(serviceURL)
 	
 	// unmarshall body of the request and populate structure currServices with information of current services from K8S API
-	err := json.Unmarshal(body, &reqDeployments)
+	err := json.Unmarshal(body, &ReqDeployments)
 	if err != nil { fmt.Println("FIRST ERROR", err)}
-	fmt.Println("\n\nUNMARSHALL", reqDeployments.Items)
-	fmt.Println("L:ENGHT", len(reqDeployments.Items))
 
+	fmt.Println("\n\nUNMARSHALL", ReqDeployments.Items)
+	fmt.Println("LENGHT", len(ReqDeployments.Items))
+
+}
+
+//FindDeployment will
+func FindDeployment() {
+	deploymentLst := ReqDeployments.Items
+	for i:=0; i < len(serviceLst); i++{
+		currService := serviceLst[i]
+		// if currService.Name == name we are looking for 
+		// Route.ServiceName = currService..Metadata.Name
+		// Route.ServicePort = currService.Spec.Ports.TargetPort
+		// Route.ServiceIP = currService.Spec.ClusterIP
+		fmt.Println("CURRENTSERVICE", currService.Metadata.Name)
+	}
 }
 
 // GetIngress contains
 func GetIngress() {
 	// GET /apis/extensions/v1beta1/namespaces/{namespace}/ingresses/{name}
-
-
 	
+}
+
+
+func Portal() {
+	///apis/revature.com/v1/namespaces/default/portals/
 }
 
 // GetResponse will request response from Kubernates API
