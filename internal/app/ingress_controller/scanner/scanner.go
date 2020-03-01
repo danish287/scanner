81a6e638ba31710191a536dbf523de82f943d1d7
Dania Estrada <ale.estrada99@gmail.com>
2020-03-01 16:58:07 -0600

    able to retreieve Target IP from the ingress

--- internal/app/ingress_controller/scanner/scanner.go
+++ internal/app/ingress_controller/scanner/scanner.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	// "log"
 	"time"
 
 	"net/http"
@@ -13,7 +12,7 @@ import (
 	"os/exec"
 )
 
-// MyServices contains
+// MyServices contains data for all services in the network
 type MyServices struct {
 	Kind       string `json:"kind"`
 	APIVersion string `json:"apiVersion"`
@@ -24,96 +23,8 @@ type MyServices struct {
 	Items []Service `json:"items"`
 }
 
-// MyDeployments contains
-type MyDeployments struct {
-	Kind       string `json:"kind"`
-	APIVersion string `json:"apiVersion"`
-	Metadata   struct {
-		SelfLink        string `json:"selfLink"`
-		ResourceVersion string `json:"resourceVersion"`
-	} `json:"metadata"`
-	Items []Deployment `json:"items"`
-}
 
-// Deployment contains
-type Deployment struct {
-	Metadata struct {
-		Name              string    `json:"name"`
-		Namespace         string    `json:"namespace"`
-		SelfLink          string    `json:"selfLink"`
-		UID               string    `json:"uid"`
-		ResourceVersion   string    `json:"resourceVersion"`
-		Generation        int       `json:"generation"`
-		CreationTimestamp time.Time `json:"creationTimestamp"`
-		Annotations       struct {
-			DeploymentKubernetesIoRevision              string `json:"deployment.kubernetes.io/revision"`
-			KubectlKubernetesIoLastAppliedConfiguration string `json:"kubectl.kubernetes.io/last-applied-configuration"`
-		} `json:"annotations"`
-	} `json:"metadata"`
-	Spec struct {
-		Replicas int `json:"replicas"`
-		Selector struct {
-			MatchLabels struct {
-				App string `json:"app"`
-			} `json:"matchLabels"`
-		} `json:"selector"`
-		Template struct {
-			Metadata struct {
-				CreationTimestamp interface{} `json:"creationTimestamp"`
-				Labels            struct {
-					App string `json:"app"`
-				} `json:"labels"`
-			} `json:"metadata"`
-			Spec struct {
-				Containers []struct {
-					Name  string `json:"name"`
-					Image string `json:"image"`
-					Ports []struct {
-						ContainerPort int    `json:"containerPort"`
-						Protocol      string `json:"protocol"`
-					} `json:"ports"`
-					Resources struct {
-					} `json:"resources"`
-					TerminationMessagePath   string `json:"terminationMessagePath"`
-					TerminationMessagePolicy string `json:"terminationMessagePolicy"`
-					ImagePullPolicy          string `json:"imagePullPolicy"`
-				} `json:"containers"`
-				RestartPolicy                 string `json:"restartPolicy"`
-				TerminationGracePeriodSeconds int    `json:"terminationGracePeriodSeconds"`
-				DNSPolicy                     string `json:"dnsPolicy"`
-				SecurityContext               struct {
-				} `json:"securityContext"`
-				SchedulerName string `json:"schedulerName"`
-			} `json:"spec"`
-		} `json:"template"`
-		Strategy struct {
-			Type          string `json:"type"`
-			RollingUpdate struct {
-				MaxUnavailable string `json:"maxUnavailable"`
-				MaxSurge       string `json:"maxSurge"`
-			} `json:"rollingUpdate"`
-		} `json:"strategy"`
-		RevisionHistoryLimit    int `json:"revisionHistoryLimit"`
-		ProgressDeadlineSeconds int `json:"progressDeadlineSeconds"`
-	} `json:"spec"`
-	Status struct {
-		ObservedGeneration  int `json:"observedGeneration"`
-		Replicas            int `json:"replicas"`
-		UpdatedReplicas     int `json:"updatedReplicas"`
-		UnavailableReplicas int `json:"unavailableReplicas"`
-		Conditions          []struct {
-			Type               string    `json:"type"`
-			Status             string    `json:"status"`
-			LastUpdateTime     time.Time `json:"lastUpdateTime"`
-			LastTransitionTime time.Time `json:"lastTransitionTime"`
-			Reason             string    `json:"reason"`
-			Message            string    `json:"message"`
-		} `json:"conditions"`
-	} `json:"status"`
-} 
-
-
-// Service contains
+// Service struct contains data pertaining to a service 
 type Service struct {
 	Metadata struct {
 		Name              string    `json:"name"`
@@ -144,14 +55,86 @@ type Service struct {
 	} `json:"status"`
 } 
 
-// Route stores
+// Portal retrieves data of portal information  
+type Portal struct {
+	APIVersion string `json:"apiVersion"`
+	Items      []struct {
+		APIVersion string `json:"apiVersion"`
+		Kind       string `json:"kind"`
+		Metadata   struct {
+			Annotations struct {
+				KubectlKubernetesIoLastAppliedConfiguration string `json:"kubectl.kubernetes.io/last-applied-configuration"`
+			} `json:"annotations"`
+			CreationTimestamp time.Time `json:"creationTimestamp"`
+			Generation        int       `json:"generation"`
+			Name              string    `json:"name"`
+			Namespace         string    `json:"namespace"`
+			ResourceVersion   string    `json:"resourceVersion"`
+			SelfLink          string    `json:"selfLink"`
+			UID               string    `json:"uid"`
+		} `json:"metadata"`
+		Spec struct {
+			Portal   string `json:"portal"`
+			Targetip string `json:"targetip"`
+		} `json:"spec"`
+	} `json:"items"`
+	Kind     string `json:"kind"`
+	Metadata struct {
+		Continue        string `json:"continue"`
+		ResourceVersion string `json:"resourceVersion"`
+		SelfLink        string `json:"selfLink"`
+	} `json:"metadata"`
+}
+
+// IngressData stores
+type IngressData struct {
+	Kind       string `json:"kind"`
+	APIVersion string `json:"apiVersion"`
+	Metadata   struct {
+		SelfLink        string `json:"selfLink"`
+		ResourceVersion string `json:"resourceVersion"`
+	} `json:"metadata"`
+	Items []struct {
+		Metadata struct {
+			Name              string    `json:"name"`
+			Namespace         string    `json:"namespace"`
+			SelfLink          string    `json:"selfLink"`
+			UID               string    `json:"uid"`
+			ResourceVersion   string    `json:"resourceVersion"`
+			Generation        int       `json:"generation"`
+			CreationTimestamp time.Time `json:"creationTimestamp"`
+			Annotations       struct {
+				NginxIngressKubernetesIoRewriteTarget string `json:"nginx.ingress.kubernetes.io/rewrite-target"`
+			} `json:"annotations"`
+		} `json:"metadata"`
+		Spec struct {
+			Rules []struct {
+				HTTP struct {
+					Paths []struct {
+						Path    string `json:"path"`
+						Backend struct {
+							ServiceName string `json:"serviceName"`
+							ServicePort int    `json:"servicePort"`
+						} `json:"backend"`
+					} `json:"paths"`
+				} `json:"http"`
+			} `json:"rules"`
+		} `json:"spec"`
+		Status struct {
+			LoadBalancer struct {
+			} `json:"loadBalancer"`
+		} `json:"status"`
+	} `json:"items"`
+}
+
+// Route stores desired route of _________
 type Route struct {
 	ServiceName string `json:"ServiceName"`
 	ServicePort string `json:"ServicePort"`
 	ServiceIP   string `json:"ServiceIP"`
 }
 
-// Rules stores
+// Rules stores ___________
 type Rules struct {
 	Protocol string `json:"Protocol"`
 	Path     string `json:"Path"`
@@ -161,16 +144,18 @@ type Rules struct {
 // Ruleset stores
 var Ruleset []Rules
 
-// ReqDeployments stores
-var ReqDeployments MyDeployments
-
 // ReqServices contians
 var ReqServices MyServices
+
+// TargetIP will store alternative IP address to dial if first one is not found
+var TargetIP []string
 
 func main() {
 	// run the kubectl proxy without TLS credentials
 	exec.Command("kubectl", "proxy", "--insecure-skip-tls-verify").Start()
-	GetServices()
+	// GetServices()
+	// GetTargetIP()
+	GetIngress()
 }
 
 // GetServices gets all of the services in our cluster from the API
@@ -189,7 +174,7 @@ func GetServices() {
 	fmt.Println("LENGHT", len(ReqServices.Items))
 }
 
-//FindService will
+//FindService searches list of services by 'name' to match 
 func FindService() {
 
 	serviceLst := ReqServices.Items
@@ -203,44 +188,40 @@ func FindService() {
 	}
 }
 
-// GetDeployments contains
-func GetDeployments() {
-	// GET /apis/apps/v1/namespaces/{namespace}/deployments/{name}
-	// request information of services from k8s API
-	serviceURL := "http://localhost:8001/apis/apps/v1/deployments"
-	body := GetResponse(serviceURL)
-	
-	// unmarshall body of the request and populate structure currServices with information of current services from K8S API
-	err := json.Unmarshal(body, &ReqDeployments)
-	if err != nil { fmt.Println("FIRST ERROR", err)}
-
-	fmt.Println("\n\nUNMARSHALL", ReqDeployments.Items)
-	fmt.Println("LENGHT", len(ReqDeployments.Items))
-
-}
-
-//FindDeployment will
-func FindDeployment() {
-	deploymentLst := ReqDeployments.Items
-	for i:=0; i < len(serviceLst); i++{
-		currService := serviceLst[i]
-		// if currService.Name == name we are looking for 
-		// Route.ServiceName = currService..Metadata.Name
-		// Route.ServicePort = currService.Spec.Ports.TargetPort
-		// Route.ServiceIP = currService.Spec.ClusterIP
-		fmt.Println("CURRENTSERVICE", currService.Metadata.Name)
-	}
-}
 
 // GetIngress contains
 func GetIngress() {
-	// GET /apis/extensions/v1beta1/namespaces/{namespace}/ingresses/{name}
 	
+	// GET /apis/extensions/v1beta1/namespaces/{namespace}/ingresses/{name}
+	// /apis/extensions/v1beta1/ingresses
+	//DreamTeamIngress
+	// items.spec, items.rules, items.http, items.path, items.sepc.ruleshttp.paths.backend.serviceport == serviceport, items.sepc.ruleshttp.paths.backend.servicename = servicename
+	//serviceip == cluster ip
+	var TargetData IngressData
+	serviceURL := "http://localhost:8001/apis/extensions/v1beta1/ingresses"
+	body := GetResponse(serviceURL)
+
+	err := json.Unmarshal(body, &TargetData)
+	if err != nil {fmt.Println(err)}
+	fmt.Println("NAME", TargetData.Items[0].Spec.Rules[0].HTTP.Paths[0].Backend.ServiceName)
+
+
+
 }
 
-
-func Portal() {
-	///apis/revature.com/v1/namespaces/default/portals/
+// GetTargetIP will retrieve targetIP from the portal to provide an alternative IP address for proxy
+func GetTargetIP() {
+	// request information of services from k8s API
+	var PortalData Portal
+	serviceURL := "http://localhost:8001/apis/revature.com/v1/namespaces/default/portals/"
+	body := GetResponse(serviceURL)
+	
+	// unmarshall body of the request and populate structure currServices with information of current services from K8S API
+	err := json.Unmarshal(body, &PortalData)
+	if err != nil { fmt.Println(err)}
+	tIP := []string{PortalData.Items[0].Spec.Targetip}
+	TargetIP = append(tIP)
+	fmt.Printf("%T", TargetIP)
 }
 
 // GetResponse will request response from Kubernates API
