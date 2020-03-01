a3f52f4c8b47f50122bf70a5632ecff492b6d93b
danish287 <dae19@duke.edu>
2020-03-01 03:25:41 -0600

    trying to read from api services

--- internal/app/ingress_controller/scanner/scanner.go
+++ internal/app/ingress_controller/scanner/scanner.go
@@ -2,12 +2,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	"net/http"
 
 	"os/exec"
 )
+
+// MyServices will
+type MyServices struct {
+	APIVersion string `json:"apiVersion"`
+	Items      []struct {
+		Metadata struct {
+			CreationTimestamp string `json:"creationTimestamp"`
+			Labels            struct {
+				Component string `json:"component"`
+				Provider  string `json:"provider"`
+			} `json:"labels"`
+			Name            string `json:"name"`
+			Namespace       string `json:"namespace"`
+			ResourceVersion string `json:"resourceVersion"`
+			SelfLink        string `json:"selfLink"`
+			UID             string `json:"uid"`
+		} `json:"metadata"`
+		Spec struct {
+			ClusterIP string `json:"clusterIP"`
+			Ports     []struct {
+				Name       string `json:"name"`
+				Port       int    `json:"port"`
+				Protocol   string `json:"protocol"`
+				TargetPort int    `json:"targetPort"`
+			} `json:"ports"`
+			SessionAffinity string `json:"sessionAffinity"`
+			Type            string `json:"type"`
+		} `json:"spec"`
+		Status struct {
+			LoadBalancer struct{} `json:"loadBalancer"`
+		} `json:"status"`
+	} `json:"items"`
+	Kind     string `json:"kind"`
+	Metadata struct {
+		ResourceVersion string `json:"resourceVersion"`
+		SelfLink        string `json:"selfLink"`
+	} `json:"metadata"`
+}
 
 // Route stores
 type Route struct {
@@ -35,10 +75,22 @@ func main() {
 
 // GetServices gets all of the services in our cluster from the API
 func GetServices() {
+	var currServices MyServices
 	// sends request to Kubernates API to retreive services
-	apiReq, err := http.NewRequest("GET", "localhost:8001/api/v1/services", nil)
+	// apiReq, err := http.NewRequest("GET", "localhost:8001/api/v1/services", nil)
+
+	response, err := http.Get("localhost:8001/api/v1/services")
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(apiReq)
+
+	body, err := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+	err = json.Unmarshal(body, &currServices)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(currServices)
 }
